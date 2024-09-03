286bd3bca1836ccb10894923fae42263bccac4ea
Mahmoud <nagboumahmoud22@gmail.com>
2024-09-03 15:04:03 +0100

    small

--- components/SmallDeviceWarning.tsx
+++ components/SmallDeviceWarning.tsx
@@ -4,11 +4,14 @@ import styles from './SmallDeviceWarning.module.css'; // Import your CSS module
 const SmallDeviceWarning: React.FC = () => {
     return (
         <div className={styles.smallDeviceOverlay}>
-            <h1 className={styles.Mahmoud}>Hi, This is Mahmoud.</h1>
-            <p className={styles.warningText}>For the best user experience, please use a larger device to explore my portfolio. Some of the projects showcased here are best viewed on a bigger screen.
+            <h1>Hi, This is <span className={styles.Mahmoud}>Mahmoud.</span></h1>
+            <p className={styles.warningText}>
+                For the best user experience, please use a larger device to explore my portfolio. Some of
+                the projects showcased here are best viewed on a bigger screen.
             </p>
-            <h4 className={styles.h4}>Visit my portfolio at:</h4> <p>https://mahmoud-nagbou-portfolio.vercel.app/</p>
-            <h3>Thank you for your understanding!</h3>
+            <h4 className={styles.h4}>Visit my portfolio at:</h4>
+            <p className={styles.portfolioLink}>https://mahmoud-nagbou-portfolio.vercel.app/</p>
+            <h3>Thank you for your understanding <span className={styles.Mahmoud}>!</span></h3>
         </div>
     );
 };
